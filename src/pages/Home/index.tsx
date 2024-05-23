4f1f9689bc4725b6c38d17df6395bce1df5cb007
Gabriel Lemos <glemoss@yahoo.com>
2024-05-23 13:45:15 -0300

    feat: home component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,40 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer>
+            <form action="submit">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput 
+                        id="task"
+                        placeholder="Dê um nome para o seu projeto"
+                    />
+
+                    <label htmlFor="minutesAmout">durante</label>
+                    <MinutesAmountInput
+                        id="minutesAmout"
+                        type="number"
+                        placeholder="00"
+                    />
+
+                    <span>minutos.</span>
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton type="submit">
+                    <Play size={24}/>
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
