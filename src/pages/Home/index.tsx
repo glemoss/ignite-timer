bef3a5b607075b5dd9ca81f3702680535709a3bc
Gabriel Lemos <glemoss@yahoo.com>
2024-05-27 12:06:34 -0300

    feat: add History component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,25 @@ export function Home() {
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput 
                         id="task"
+                        list="task-suggestions"
                         placeholder="Dê um nome para o seu projeto"
                     />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto-1" />
+                        <option value="Projeto-2" />
+                        <option value="Projeto-3" />
+                        <option value="Projeto-4" />
+                    </datalist>
 
                     <label htmlFor="minutesAmout">durante</label>
                     <MinutesAmountInput
                         id="minutesAmout"
                         type="number"
                         placeholder="00"
+                        step={5}
+                        min={0}
+                        max={60}
                     />
 
                     <span>minutos.</span>
