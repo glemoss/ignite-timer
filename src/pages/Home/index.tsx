1da85c9a4c1d905072b15bb1a36662d313ec7151
Gabriel Lemos <glemoss@yahoo.com>
2024-05-31 10:55:03 -0300

    feat: new cycle countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -32,6 +32,8 @@ interface Cycle {
 
 export function Home() {
     const [cycles, setCycles] = useState<Cycle[]>([]);
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
     const { register, handleSubmit, watch, reset } = useForm<CreateNewCycleFormData>({
         resolver: zodResolver(createNewCycleSchema),
@@ -42,16 +44,30 @@ export function Home() {
     });
 
     function handleCreateNewCycle(data: CreateNewCycleFormData) {
+        const id = String(new Date().getTime());
+
         const newCycle: Cycle = {
-            id: String(new Date().getTime()),
+            id,
             task: data.task,
             minutesAmount: data.minutesAmount,
         }
 
         setCycles((state) => [...state, newCycle]);
+        setActiveCycleId(id);
 
         reset();
     }
+
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+    const totalTimeInSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+    const secondsLeft = activeCycle ? totalTimeInSeconds - amountSecondsPassed : 0;
+
+    const minutesAmount = Math.floor(secondsLeft / 60);
+    const secondsAmount = secondsLeft % 60;
+
+    const minutes = String(minutesAmount).padStart(2, '0');
+    const seconds = String(secondsAmount).padStart(2, '0');
 
     const task = watch('task')
     const isSubmitDisabled = task
@@ -90,11 +106,11 @@ export function Home() {
                 </FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
                 <StartCountdownButton type="submit" disabled={!isSubmitDisabled}>
